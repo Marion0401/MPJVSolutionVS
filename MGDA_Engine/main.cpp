58fafe878a1f18715aca31aea45b7ce4534d5f08
Damien-Joseph RISPAL <damienjoseph.rispal@gmail.com>
2022-10-27 18:22:47 -0400

    Porting the game to glfw

--- MGDA_Engine/main.cpp
+++ MGDA_Engine/main.cpp
@@ -1,184 +1,227 @@
-#include <SFML/Graphics.hpp>
+#include <GLFW/glfw3.h>
 #include "./Game/SimpleShooter/Bullet.h"
 #include "./Game/SimpleShooter/Cannonball.h"
 #include "./Game/SimpleShooter/Fireball.h"
 #include "./Game/SimpleShooter/Lazer.h"
+#include "./Game/SimpleShooter/GameEnvironment.h"
 #include <iostream>
 
 #define CONST_GRAPHIQUE 10
 #define LEN_CIBLE 200
 
-using namespace sf;
+#define W_WIDTH 1920
+#define W_HEIGHT 1080
+
 #include <time.h>
 
-int main()
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
+	GameEnvironment* env = reinterpret_cast<GameEnvironment*>(glfwGetWindowUserPointer(window));
+	if (env)
+	{
+		//Update Mouvement de la Balle
+		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+			env->isShot = true;
+	}
+}
 
-	RenderWindow window(VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "Game");
+void processInput(GLFWwindow* window)
+{
+	GameEnvironment* environment = reinterpret_cast<GameEnvironment*>(glfwGetWindowUserPointer(window));
+	if (!environment)
+	{
+		return;
+	}
+
+	// Keyboard
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	// Changement de projectile
+		// 1 Fireball
+		// 2 Bullet
+		// 3 Cannonbullet
+		// 4 Lazer
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+	{
+		environment->projCurrent = Fireball(Vector3D(0, 0, 0), Vector3D(-1, 0, 0), Vector3D(0, 0, 0));
+		environment->projCurrent.setVelocity(Vector3D(-fireball_velocity, 0, 0));
+		ball.setFillColor(Color::Red);
+		nom_Proj.setString("Fireball");
+		info_projectile.setString("Velocity = " + std::to_string(environment->projCurrent.getVelocity().getNorm()) + "\n"
+			"Mass = " + std::to_string(environment->projCurrent.getMass()) + "\n"
+			"Radius = " + std::to_string(environment->projCurrent.getRadius()) + "\n");
+
+		ball.setRadius(environment->projCurrent.getRadius() * CONST_GRAPHIQUE);
+		ball.setPosition(start_ball, 0);
+		environment->isShot = false;
+
+	}
+	else if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+		environment->projCurrent = Bullet(Vector3D(0, 0, 0), Vector3D(-1, 0, 0), Vector3D(0, 0, 0));
+		environment->projCurrent.setVelocity(Vector3D(-bullet_velocity, 0, 0));
+		ball.setFillColor(Color::Green);
+		nom_Proj.setString("Bullet");
+		info_projectile.setString("Velocity = " + std::to_string(environment->projCurrent.getVelocity().getNorm()) + "\n"
+			"Mass = " + std::to_string(environment->projCurrent.getMass()) + "\n"
+			"Radius = " + std::to_string(environment->projCurrent.getRadius()) + "\n");
+
+		ball.setRadius(environment->projCurrent.getRadius() * CONST_GRAPHIQUE);
+		ball.setPosition(start_ball, 0);
+		environment->isShot = false;
+
+	}
+	else if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+		environment->projCurrent = Cannonball(Vector3D(0, 0, 0), Vector3D(-1, 0, 0), Vector3D(0, 0, 0));
+		environment->projCurrent.setVelocity(Vector3D(-cannonball_velocity, 0, 0));
+		ball.setFillColor(Color::Blue);
+		nom_Proj.setString("Cannonball");
+		info_projectile.setString("Velocity = " + std::to_string(environment->projCurrent.getVelocity().getNorm()) + "\n"
+			"Mass = " + std::to_string(environment->projCurrent.getMass()) + "\n"
+			"Radius = " + std::to_string(environment->projCurrent.getRadius()) + "\n");
+
+		ball.setRadius(environment->projCurrent.getRadius() * CONST_GRAPHIQUE);
+		ball.setPosition(start_ball, 0);
+		environment->isShot = false;
+
+	}
+	else if (glfwGetKey(window, GLFW_KEY_4) == GLFW_PRESS) {
+		environment->projCurrent = Lazer(Vector3D(0, 0, 0), Vector3D(-1, 0, 0), Vector3D(0, 0, 0));
+		environment->projCurrent.setVelocity(Vector3D(-lazer_velocity, 0, 0));
+		ball.setFillColor(Color::Yellow);
+		nom_Proj.setString("Lazer");
+		info_projectile.setString("Velocity = " + std::to_string(environment->projCurrent.getVelocity().getNorm()) + "\n"
+			"Mass = " + std::to_string(environment->projCurrent.getMass()) + "\n"
+			"Radius = " + std::to_string(environment->projCurrent.getRadius()) + "\n");
+
+		ball.setRadius(environment->projCurrent.getRadius() * CONST_GRAPHIQUE);
+		ball.setPosition(start_ball, 0);
+		environment->isShot = false;
+
+	}
+
+}
+
+int main(void)
+{
+	GLFWwindow* window;
+	GameEnvironment* environment = new GameEnvironment();
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "Simple Shooter", glfwGetPrimaryMonitor(), NULL);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwSetWindowUserPointer(window, reinterpret_cast<void*>(environment));
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	// Mouse
+	glfwSetMouseButtonCallback(window,mouse_button_callback);
+
 	time_t t1, t2;
 	float deltatime = 0;
 
     // Setup du projectile
-    Projectile projCurrent = Fireball(Vector3D(0,0,0), Vector3D(-1, 0, 0), Vector3D(0,0,0));
+    environment->projCurrent = Fireball(Vector3D(0,0,0), Vector3D(-1, 0, 0), Vector3D(0,0,0));
 
     //Setup balle qui représente le projectile
-	CircleShape ball;
-	bool isShot = false;
-	ball.setRadius(projCurrent.getRadius() * CONST_GRAPHIQUE);
-	ball.setFillColor(Color::Red);
-	ball.setPosition(window.getSize().x - (projCurrent.getRadius() * CONST_GRAPHIQUE)*2, 0);
+	//CircleShape ball;
+	//bool environment->isShot = false;
+	//ball.setRadius(environment->projCurrent.getRadius() * CONST_GRAPHIQUE);
+	//ball.setFillColor(Color::Red);
+	//ball.setPosition(window.getSize().x - (environment->projCurrent.getRadius() * CONST_GRAPHIQUE)*2, 0);
 
 	//Setup target
-	RectangleShape cible(sf::Vector2f(20, 100));
-	int dir = 0;
-	cible.setFillColor(Color::Red);
-	cible.setPosition(10, LEN_CIBLE);
+	//RectangleShape cible(sf::Vector2f(20, 100));
+	//int dir = 0;
+	//cible.setFillColor(Color::Red);
+	//cible.setPosition(10, LEN_CIBLE);
 
     // Interface du jeu
-    sf::Font font;
-	if (!font.loadFromFile("./resources/Font/Optien.ttf"))
-	{
-		std::cerr << ".Error while loading font" << std::endl;
-		return -1;
-	}
+    //sf::Font font;
+	//if (!font.loadFromFile("./resources/Font/Optien.ttf"))
+	//{
+	//	std::cerr << ".Error while loading font" << std::endl;
+	//	return -1;
+	//}
 
 	//Nom du projectile
-    sf::Text nom_Proj;
-    nom_Proj.setFont(font);
-    nom_Proj.setString("Fireball");
-    nom_Proj.setCharacterSize(50);
-    nom_Proj.setFillColor(sf::Color::White);
-    nom_Proj.setStyle(sf::Text::Bold | sf::Text::Underlined);
-    nom_Proj.setPosition(40,0);
+    //sf::Text nom_Proj;
+    //nom_Proj.setFont(font);
+    //nom_Proj.setString("Fireball");
+    //nom_Proj.setCharacterSize(50);
+    //nom_Proj.setFillColor(sf::Color::White);
+    //nom_Proj.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    //nom_Proj.setPosition(40,0);
 
     //Informations du jeu
-    sf::Text info_jeu;
-    info_jeu.setFont(font);
-    info_jeu.setString("Press 1 Fireball - 2 pts\n"
-    		"Press 2 Bullet - 10 pts\n"
-    		"Press 3 Cannonball - 5 pts\n"
-    		"Press 4 Lazer - 1 pts");
-    info_jeu.setCharacterSize(20);
-    info_jeu.setPosition(50,60);
-    info_jeu.setFillColor(sf::Color::White);
+    //sf::Text info_jeu;
+    //info_jeu.setFont(font);
+    //info_jeu.setString("Press 1 Fireball - 2 pts\n"
+    //		"Press 2 Bullet - 10 pts\n"
+    //		"Press 3 Cannonball - 5 pts\n"
+    //		"Press 4 Lazer - 1 pts");
+    //info_jeu.setCharacterSize(20);
+    //info_jeu.setPosition(50,60);
+    //info_jeu.setFillColor(sf::Color::White);
 
     // Informations du projectile
-    sf::Text info_projectile;
-    info_projectile.setFont(font);
-    info_projectile.setString("Velocity = " + std::to_string(projCurrent.getVelocity().getNorm()) + "\n"
-								"Mass = " + std::to_string(projCurrent.getMass()) + "\n"
-								"Radius = " + std::to_string(projCurrent.getRadius()) + "\n");
-    info_projectile.setCharacterSize(20);
-    info_projectile.setPosition(350,60);
-    info_projectile.setFillColor(sf::Color::White);
+    //sf::Text info_projectile;
+    //info_projectile.setFont(font);
+    //info_projectile.setString("Velocity = " + std::to_string(environment->projCurrent.getVelocity().getNorm()) + "\n"
+	//							"Mass = " + std::to_string(environment->projCurrent.getMass()) + "\n"
+	//							"Radius = " + std::to_string(environment->projCurrent.getRadius()) + "\n");
+    //info_projectile.setCharacterSize(20);
+    //info_projectile.setPosition(350,60);
+    //info_projectile.setFillColor(sf::Color::White);
 
-    while (window.isOpen())
+    while (!glfwWindowShouldClose(window))
     {
+		//Input
+		processInput(window);
+
     	//calcul du temps d'une frame
     	t1 = time(nullptr);
 
-
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
     	// Offset_y du nouveau projectile quand on le change
-    	int start_ball = window.getSize().x - (projCurrent.getRadius() * CONST_GRAPHIQUE)*2;
+    	int start_ball = W_WIDTH - (environment->projCurrent.getRadius() * CONST_GRAPHIQUE)*2;
 
-        // Changement de projectile
-        // 1 Fireball
-        // 2 Bullet
-        // 3 Cannonbullet
-        // 4 Lazer
-        if(Keyboard::isKeyPressed(sf::Keyboard::Num1))
-		{
-			projCurrent = Fireball(Vector3D(0,0,0), Vector3D(-1, 0, 0), Vector3D(0,0,0));
-			projCurrent.setVelocity(Vector3D(-fireball_velocity, 0, 0));
-			ball.setFillColor(Color::Red);
-			nom_Proj.setString("Fireball");
-			info_projectile.setString("Velocity = " + std::to_string(projCurrent.getVelocity().getNorm()) + "\n"
-										"Mass = " + std::to_string(projCurrent.getMass()) + "\n"
-										"Radius = " + std::to_string(projCurrent.getRadius()) + "\n");
-
-			ball.setRadius(projCurrent.getRadius() * CONST_GRAPHIQUE);
-			ball.setPosition(start_ball , 0);
-			isShot = false;
-
-		} else if(Keyboard::isKeyPressed(sf::Keyboard::Num2)) {
-			projCurrent = Bullet(Vector3D(0,0,0), Vector3D(-1, 0, 0), Vector3D(0,0,0));
-			projCurrent.setVelocity(Vector3D(-bullet_velocity, 0, 0));
-			ball.setFillColor(Color::Green);
-			nom_Proj.setString("Bullet");
-			info_projectile.setString("Velocity = " + std::to_string(projCurrent.getVelocity().getNorm()) + "\n"
-										"Mass = " + std::to_string(projCurrent.getMass()) + "\n"
-										"Radius = " + std::to_string(projCurrent.getRadius()) + "\n");
-
-			ball.setRadius(projCurrent.getRadius() * CONST_GRAPHIQUE);
-			ball.setPosition(start_ball , 0);
-			isShot = false;
-
-		} else if(Keyboard::isKeyPressed(sf::Keyboard::Num3)) {
-			projCurrent = Cannonball(Vector3D(0,0,0), Vector3D(-1, 0, 0), Vector3D(0,0,0));
-			projCurrent.setVelocity(Vector3D(-cannonball_velocity, 0, 0));
-			ball.setFillColor(Color::Blue);
-			nom_Proj.setString("Cannonball");
-			info_projectile.setString("Velocity = " + std::to_string(projCurrent.getVelocity().getNorm()) + "\n"
-										"Mass = " + std::to_string(projCurrent.getMass()) + "\n"
-										"Radius = " + std::to_string(projCurrent.getRadius()) + "\n");
-
-			ball.setRadius(projCurrent.getRadius() * CONST_GRAPHIQUE);
-			ball.setPosition(start_ball , 0);
-			isShot = false;
-
-		} else if(Keyboard::isKeyPressed(sf::Keyboard::Num4)) {
-			projCurrent = Lazer(Vector3D(0,0,0), Vector3D(-1, 0, 0), Vector3D(0,0,0));
-			projCurrent.setVelocity(Vector3D(-lazer_velocity, 0, 0));
-			ball.setFillColor(Color::Yellow);
-			nom_Proj.setString("Lazer");
-			info_projectile.setString("Velocity = " + std::to_string(projCurrent.getVelocity().getNorm()) + "\n"
-										"Mass = " + std::to_string(projCurrent.getMass()) + "\n"
-										"Radius = " + std::to_string(projCurrent.getRadius()) + "\n");
-
-			ball.setRadius(projCurrent.getRadius() * CONST_GRAPHIQUE);
-			ball.setPosition(start_ball , 0);
-			isShot = false;
-
-		}
-
-        //Update Mouvement de la Balle
-        if(Mouse::isButtonPressed(Mouse::Left))
-			isShot = true;
-
-        if (!isShot)
+        if (!environment->isShot)
         {
         	ball.setPosition(ball.getPosition().x, Mouse::getPosition(window).y);
-        	projCurrent.setX(ball.getPosition().x);
-        	projCurrent.setY(Mouse::getPosition(window).y);
+        	environment->projCurrent.setX(ball.getPosition().x);
+        	environment->projCurrent.setY(Mouse::getPosition(window).y);
 
         } else {
-        	//std::cout << projCurrent.getVelocity() << std::endl;
+        	//std::cout << environment->projCurrent.getVelocity() << std::endl;
 
         	// TODO corriger pb deltatime = 0
-        	projCurrent.integrer(0.05);
-        	ball.setPosition(projCurrent.getX(), projCurrent.getY());
+        	environment->projCurrent.integrer(0.05);
+        	ball.setPosition(environment->projCurrent.getX(), environment->projCurrent.getY());
 
-        	//ball.move(projCurrent.getVelocity().getX(), projCurrent.getVelocity().getY());
+        	//ball.move(environment->projCurrent.getVelocity().getX(), environment->projCurrent.getVelocity().getY());
 
         }
 
         if(ball.getPosition().x <= 0)
 		{
-			isShot = false;
-		    ball.setPosition(Vector2f(window.getSize().x - ball.getRadius()*3, ball.getPosition().y));
+			environment->isShot = false;
+		    ball.setPosition(Vector2f(W_WIDTH - ball.getRadius()*3, ball.getPosition().y));
 		}
 
         //Update Mouvement de la Cible
 		if (cible.getPosition().y <= 0)
 			dir = 1;
-		else if (cible.getPosition().y + LEN_CIBLE/2 >= window.getSize().y)
+		else if (cible.getPosition().y + LEN_CIBLE/2 >= W_HEIGHT)
 			dir = 0;
 
 		if (dir == 0)
@@ -190,16 +233,10 @@ int main()
 			cible.move(0, 1.f);
 		}
 
-        window.clear(Color::Black);
-
-        //Draw
-        window.draw(cible);
-        window.draw(ball);
-        window.draw(nom_Proj);
-        window.draw(info_jeu);
-        window.draw(info_projectile);
-
-        window.display();
+		/* Draw */
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 
         //calcul delta d'une frame
         t2 = time(nullptr);
@@ -208,6 +245,7 @@ int main()
 
     }
 
-    return 0;
+	glfwTerminate();
+	return 0;
 }
 
